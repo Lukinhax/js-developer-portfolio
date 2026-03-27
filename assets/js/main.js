e2e88423b24976026481b557e01353e0d27b1ae1
lucas <lucasmamelo@hotmail.com>
2026-03-27 17:16:55 -0300

    subindo a seção de idiomas

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,18 +25,23 @@ function updateProfileInfo(profileData) {
 function updateSoftSkills(profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills')
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
-
 }
 
 function updateHardSkills(profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills')
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
-
 }
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.idiomas.map(language => `<li>${language}</li>`).join('')
+}
+
 
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
+    updateLanguages(profileData)
 })()
