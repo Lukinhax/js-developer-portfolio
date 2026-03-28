19f91acab8282579c27fa398d2f1d5396467f3b1
lucas <lucasmamelo@hotmail.com>
2026-03-28 13:16:45 -0300

    corrigindo erro no portifolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,7 @@ function updatePortfolio(profileData) {
     const portfolio = document.getElementById('profile.portfolio')
     portfolio.innerHTML = profileData.portfolio.map(project => `
         <li>
-            <h3 ${project.github ? 'class="github"' : ''}>${project.nome}</h3>
+            <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
             <a href="${project.url}" target="_blank">${project.url}</a>
         </li>
     `).join('')
