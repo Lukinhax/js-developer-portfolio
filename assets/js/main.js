9c391722d37c180e3587ee69414baaf0dee5d420
lucas <lucasmamelo@hotmail.com>
2026-03-28 13:03:13 -0300

    subindo a seção professional experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,17 @@ function updatePortfolio(profileData) {
     `).join('')
 }
 
+function updateProfessionalExperience(profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => `
+        <li>
+            <h3 class="title">${experience.name}</h3>
+            <p class="period">${experience.period}</p>
+            <p>${experience.description}</p>
+        </li>
+    `).join('')
+}
+
 
 (async () => {
     const profileData = await fetchProfileData()
